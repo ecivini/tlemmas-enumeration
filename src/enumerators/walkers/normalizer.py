165e489741c654028363000f248a30dc86b14981
Gabriele Masina <gabriele.masina@virgilio.it>
2026-05-18 16:12:59 +0200

    Simplify walker implementations (#4)
    * refactor walkers 
    * add tests

--- src/enumerators/walkers/normalizer.py
+++ src/enumerators/walkers/normalizer.py
@@ -1,88 +1,35 @@
-'''this module defines a Walker that takes a pysmt formula and normalizes its atoms'''
+"""this module defines a Walker that takes a pysmt formula and normalizes its atoms"""
 
-from pysmt.walkers import DagWalker, handles
 import pysmt.operators as op
 from pysmt.fnode import FNode
-
-from pysmt.shortcuts import And, Or, Iff, Implies, TRUE, FALSE, Not, Ite
-
-from enumerators.util.custom_exceptions import UnsupportedNodeException
+from pysmt.formula import FormulaManager
+from pysmt.walkers import DagWalker, handles
 
 
 class NormalizerWalker(DagWalker):
-    '''A walker to normalize smt formulas acccording to a converter'''
+    """A walker to normalize smt formulas according to a converter"""
 
     def __init__(self, converter, env=None, invalidate_memoization=False):
         DagWalker.__init__(self, env, invalidate_memoization)
         self._converter = converter
         return
 
-    def walk_and(self, formula: FNode, args, **kwargs):
-        '''translate AND node'''
-        # pylint: disable=unused-argument
-        return And(*args)
+    @property
+    def mgr(self) -> FormulaManager:
+        return self.env.formula_manager
 
-    def walk_or(self, formula: FNode, args, **kwargs):
-        '''translate OR node'''
-        # pylint: disable=unused-argument
-        return Or(*args)
-
-    def walk_not(self, formula: FNode, args, **kwargs):
-        '''translate NOT node'''
-        # pylint: disable=unused-argument
-        return Not(args[0])
-
-    def walk_symbol(self, formula: FNode, args, **kwargs):
-        '''translate SYMBOL node'''
-        # pylint: disable=unused-argument
-        return self._convert(formula)
-
-    def walk_bool_constant(self, formula: FNode, args, **kwargs):
-        '''translate BOOL const node'''
-        # pylint: disable=unused-argument
-        value = formula.constant_value()
-        if value:
-            return TRUE()
-        return FALSE()
-
-    def walk_iff(self, formula, args, **kwargs):
-        '''translate IFF node'''
-        # pylint: disable=unused-argument
-        return Iff(args[0], args[1])
-
-    def walk_implies(self, formula, args, **kwargs):
-        '''translate IMPLIES node'''  # a -> b === (~ a) v b
-        # pylint: disable=unused-argument
-        return Implies(args[0], args[1])
-
-    def walk_ite(self, formula, args, **kwargs):
-        '''translate ITE node'''
-        # pylint: disable=unused-argument
-        return Ite(args[0], args[1], args[2])
-
-    def _convert(self, formula):
+    @handles(*op.RELATIONS, op.FUNCTION)
+    def walk_theory(self, formula: FNode, args: tuple[FNode], **kwargs) -> FNode:
+        # Theory atoms get normalized
         msat_term = self._converter.convert(formula)
         return self._converter.back(msat_term)
 
-    def walk_forall(self, formula, args, **kwargs):
-        '''translate For-all node'''
-        # pylint: disable=unused-argument
-        raise UnsupportedNodeException('Quantifiers are yet to be supported')
+    @handles(op.SYMBOL, *op.CONSTANTS, *op.THEORY_OPERATORS)
+    def walk_noop(self, formula: FNode, args: tuple[FNode], **kwargs) -> FNode:
+        # symbols and constants do not change
+        return formula
 
-    def walk_exists(self, formula, args, **kwargs):
-        '''translate Exists node'''
-        # pylint: disable=unused-argument
-        raise UnsupportedNodeException('Quantifiers are yet to be supported')
-
-    @handles(op.EQUALS)
-    def walk_equals(self, formula, args, **kwargs):
-        '''translate Equals node'''
-        # pylint: disable=unused-argument
-        return self._convert(formula)
-
-    @handles(*op.THEORY_OPERATORS, *op.BV_RELATIONS, *op.IRA_RELATIONS,
-             *op.STR_RELATIONS, op.REAL_CONSTANT, op.BV_CONSTANT, op.INT_CONSTANT, op.FUNCTION)
-    def walk_theory(self, formula, args, **kwargs):
-        '''translate theory node'''
-        # pylint: disable=unused-argument
-        return self._convert(formula)
+    @handles(*op.BOOL_CONNECTIVES, op.ITE)
+    def walk_bool_op(self, formula: FNode, args: tuple[FNode], **kwargs) -> FNode:
+        # Boolean connectives just connect normalized children
+        return self.mgr.create_node(formula.node_type(), tuple(args))
--- src/enumerators/walkers/walker_bool_abstraction.py
+++ src/enumerators/walkers/walker_bool_abstraction.py
@@ -1,83 +1,49 @@
-from pysmt.walkers import DagWalker, handles
 import pysmt.operators as op
+from pysmt.environment import Environment
 from pysmt.fnode import FNode
-
-from pysmt.shortcuts import And, Or, Iff, Implies, TRUE, FALSE, Not, Ite, BOOL
-
-from enumerators.util.custom_exceptions import UnsupportedNodeException
+from pysmt.formula import FormulaManager
+from pysmt.typing import BOOL
+from pysmt.walkers import DagWalker, handles
 
 
 class BooleanAbstractionWalker(DagWalker):
-    '''A walker to normalize smt formulas into its boolean abstraction'''
+    """A walker to normalize smt formulas into its boolean abstraction"""
 
-    def __init__(self, atoms=None, abstraction=None, env=None, invalidate_memoization=False):
+    def __init__(
+        self,
+        atoms: list[FNode] | None = None,
+        abstraction: dict[FNode, FNode] | None = None,
+        env: Environment | None = None,
+        invalidate_memoization: bool = False,
+    ):
         DagWalker.__init__(self, env, invalidate_memoization)
-        self.atoms = atoms if atoms is not None else {}
+        self.atoms = atoms if atoms is not None else []
         self.abstraction = abstraction if abstraction is not None else {}
 
         for atom in self.atoms:
             self._abstract(atom)
 
-    def walk_and(self, formula: FNode, args, **kwargs):
-        '''translate AND node'''
-        # pylint: disable=unused-argument
-        return And(*args)
+    @property
+    def mgr(self) -> FormulaManager:
+        return self.env.formula_manager
 
-    def walk_or(self, formula: FNode, args, **kwargs):
-        '''translate OR node'''
-        # pylint: disable=unused-argument
-        return Or(*args)
-
-    def walk_not(self, formula: FNode, args, **kwargs):
-        '''translate NOT node'''
-        # pylint: disable=unused-argument
-        return Not(args[0])
-
-    def walk_symbol(self, formula: FNode, args, **kwargs):
-        '''translate SYMBOL node'''
-        # pylint: disable=unused-argument
-        return formula
-
-    def walk_iff(self, formula, args, **kwargs):
-        '''translate IFF node'''
-        # pylint: disable=unused-argument
-        return Iff(args[0], args[1])
-
-    def walk_implies(self, formula, args, **kwargs):
-        '''translate IMPLIES node'''  # a -> b === (~ a) v b
-        # pylint: disable=unused-argument
-        return Implies(args[0], args[1])
-
-    def walk_ite(self, formula, args, **kwargs):
-        '''translate ITE node'''
-        # pylint: disable=unused-argument
-        return Ite(args[0], args[1], args[2])
-
-    def _abstract(self, formula):
+    def _abstract(self, formula: FNode) -> FNode:
         if formula not in self.abstraction:
             var_name = f"v{len(self.abstraction)}"
-            abstr_var = self.env.formula_manager.Symbol(var_name, BOOL)
+            abstr_var = self.mgr.Symbol(var_name, BOOL)
             self.abstraction[formula] = abstr_var
         return self.abstraction[formula]
 
-    def walk_forall(self, formula, args, **kwargs):
-        '''translate For-all node'''
-        # pylint: disable=unused-argument
-        raise UnsupportedNodeException('Quantifiers are yet to be supported')
+    @handles(*op.RELATIONS, op.FUNCTION)
+    def walk_theory(self, formula: FNode, args: tuple[FNode], **kwargs) -> FNode:
+        return self._abstract(formula)
 
-    def walk_exists(self, formula, args, **kwargs):
-        '''translate Exists node'''
-        # pylint: disable=unused-argument
-        raise UnsupportedNodeException('Quantifiers are yet to be supported')
-
-    @handles(*op.CONSTANTS, *op.THEORY_OPERATORS)
-    def walk_constants(self, formula, args, **kwargs):
-        '''translate Equals node'''
-        # pylint: disable=unused-argument
+    @handles(op.SYMBOL, *op.CONSTANTS, *op.THEORY_OPERATORS)
+    def walk_noop(self, formula: FNode, args: tuple[FNode], **kwargs) -> FNode:
+        # symbols and constants do not change
         return formula
 
-    @handles(*op.RELATIONS, op.FUNCTION)
-    def walk_theory(self, formula, args, **kwargs):
-        '''translate theory node'''
-        # pylint: disable=unused-argument
-        return self._abstract(formula)
+    @handles(*op.BOOL_CONNECTIVES, op.ITE)
+    def walk_bool_op(self, formula: FNode, args: tuple[FNode], **kwargs) -> FNode:
+        # Boolean connectives just connect normalized children
+        return self.mgr.create_node(formula.node_type(), tuple(args))
