e6e35e76227ca68a73b135c6acac19f8ee32348f
Gabriele Masina <gabriele.masina@virgilio.it>
2026-05-20 15:12:34 +0200

    Merge tests for abstraction and refinement

--- tests/walkers/test_boolean_abstraction.py
+++ tests/walkers/test_boolean_abstraction.py
@@ -1,18 +1,51 @@
 from pysmt.fnode import FNode
 from pysmt.shortcuts import is_valid
+from pysmt.typing import BOOL
 from enumerators.walkers.walker_bool_abstraction import BooleanAbstractionWalker
+from enumerators.walkers.walker_refinement import RefinementWalker
 
 
 def test_bool_abstraction_walker(i, j, k):
-    or_1 = i.Equals(10).Implies(j.Equals(0)) | i.Equals(0).Implies(j.Equals(10))
-    or_2 = j.Equals(10).Implies(k.Equals(0)) | j.Equals(0).Implies(k.Equals(10))
-
-    formula: FNode = or_1 & or_2
+    formula: FNode = (i.Equals(10).Implies(j.Equals(0)) | i.Equals(0).Implies(j.Equals(10))) & (
+        j.Equals(10).Implies(k.Equals(0)) | j.Equals(0).Implies(k.Equals(10))
+    )
 
     abswalker = BooleanAbstractionWalker()
     abstracted_formula = abswalker.abstract(formula)
 
-    assert len(abswalker.abstraction) == 6, "There should be 6 abstracted atoms"
+    assert len(abswalker.abstraction) == 6
+    assert abstracted_formula == formula.substitute(abswalker.abstraction)
+    assert all(atom.is_symbol(BOOL) for atom in abstracted_formula.get_atoms())
+
+
+def test_bool_abstraction_preserves_boolean_atoms(a, x, y):
+    formula: FNode = a & (x.Equals(1) | y.Equals(2))
+
+    abswalker = BooleanAbstractionWalker()
+    abstracted_formula = abswalker.abstract(formula)
 
     expected_abstracted = formula.substitute(abswalker.abstraction)
-    assert is_valid(abstracted_formula.Iff(expected_abstracted)), "Abstracted formula does not match expected structure"
+    assert a in abstracted_formula.get_atoms()
+    assert is_valid(abstracted_formula.Iff(expected_abstracted))
+
+
+def test_boolean_abstraction_and_refinement_round_trip(x, y, a):
+    phi: FNode = a | ((x.Equals(10) | y.Equals(0)) & x.Equals(0))
+    phi_atoms: set[FNode] = phi.get_atoms()
+
+    abstr_walker = BooleanAbstractionWalker()
+    abstr_phi: FNode = abstr_walker.abstract(phi)
+    abstraction = abstr_walker.abstraction
+
+    abstr_phi_atoms: set[FNode] = abstr_phi.get_atoms()
+    assert len(abstraction) == 3
+    assert all(atom.is_symbol(BOOL) for atom in abstr_phi_atoms)
+    assert all(abstr.is_symbol(BOOL) for abstr in abstraction.values())
+
+    bool_atoms = {atom for atom in phi_atoms if atom.is_symbol(BOOL)}
+    assert bool_atoms <= abstr_phi_atoms
+
+    refinement_walker = RefinementWalker(abstraction=abstraction)
+    refined_phi = refinement_walker.refine(abstr_phi)
+
+    assert phi == refined_phi, "Refinement should recover the original formula"
