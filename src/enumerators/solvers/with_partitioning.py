24bb41fe86b54c469447daf3313f09c9889524f8
Gabriele Masina <gabriele.masina@virgilio.it>
2026-05-24 16:41:01 +0200

    Fix: partition conjoined components based on free-variables sharing

--- src/enumerators/solvers/with_partitioning.py
+++ src/enumerators/solvers/with_partitioning.py
@@ -52,20 +52,35 @@ def get_conjoined_components(
     phi: FNode,
 ) -> tuple[dict[FNode, frozenset[FNode]], dict[FNode, set[FNode]]]:
     """
-    Flattens a formula and maps components to their atoms, and atoms to their components.
+    Flattens a formula and groups components that share free variables.
     """
     components = AndFlattener().flatten(phi)
 
-    component_to_atoms: dict[FNode, frozenset[FNode]] = {}
-    atom_to_components: defaultdict[FNode, set[FNode]] = defaultdict(set)
+    var_owner: dict[FNode, FNode] = {}
+    uf = UnionFind()
 
     for c in components:
-        # Precompute the theory atoms for this component once
-        atoms = frozenset(get_theory_atoms(c.get_atoms()))
-        component_to_atoms[c] = atoms
+        for var in c.get_free_variables():
+            owner = var_owner.get(var)
+            if owner is None:
+                var_owner[var] = c
+            else:
+                uf.union(c, owner)
+
+    root_to_components: defaultdict[FNode, list[FNode]] = defaultdict(list)
+    for component in components:
+        root_to_components[uf.find(component)].append(component)
+
+    component_to_atoms: dict[FNode, frozenset[FNode]] = {}
+    atom_to_components: defaultdict[FNode, set[FNode]] = defaultdict(set)
+    for root_components in root_to_components.values():
+        with SuspendTypeChecking():
+            component = And(*root_components)
+        atoms = frozenset(get_theory_atoms(component.get_atoms()))
+        component_to_atoms[component] = atoms
 
         for atom in atoms:
-            atom_to_components[atom].add(c)
+            atom_to_components[atom].add(component)
 
     return component_to_atoms, atom_to_components
 
