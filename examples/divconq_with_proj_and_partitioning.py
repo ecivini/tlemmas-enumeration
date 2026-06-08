18a7aab4b87ae66ea5a6eac5df2ce82fe55a7a9e
Gabriele Masina <gabriele.masina@virgilio.it>
2026-06-08 16:36:57 +0200

    Minors

--- examples/divconq_with_proj_and_partitioning.py
+++ examples/divconq_with_proj_and_partitioning.py
@@ -1,7 +1,6 @@
 from pysmt.shortcuts import Symbol, REAL
 from enumerators.solvers.mathsat_divide_and_conquer import MathSATDivideAndConquerEnumerator
 from enumerators.solvers.with_partitioning import WithPartitioningWrapper
-from enumerators.solvers.with_projection import WithProjectionWrapper
 
 # Create variables
 x = Symbol("x", REAL)
@@ -12,9 +11,7 @@ phi = (x + y >= 1) | (x + y <= 0)
 
 # Enumerate using AllSMT enumeration
 parallel_workers = 4
-enumerator = WithPartitioningWrapper(
-    WithProjectionWrapper(MathSATDivideAndConquerEnumerator(parallel_procs=parallel_workers))
-)
+enumerator = WithPartitioningWrapper(MathSATDivideAndConquerEnumerator(parallel_procs=parallel_workers))
 result = enumerator.check_all_sat(phi)
 
 # Get the theory lemmas
